ce60082f6064c73499d0b79c9ab03b5b5d538062
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-05-11 20:42:22 +0200

    disable 'add' button when any field is empty, complete validation

---
+++ Contact Manager/Converters/EmptyStringToBooleanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Contact_Manager.Converters
+{
+    [ValueConversion(typeof(string), typeof(bool))]
+    public class EmptyStringToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string str = value as string;
+
+            return string.IsNullOrEmpty(str);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
