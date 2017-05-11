f48c1714097ecf36bc4c2d995b6f8de40db5eb1e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-05-11 20:33:58 +0200

    bind the 'add' button to the errors

---
+++ Contact Manager/Converters/NorBooleanConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Contact_Manager.Converters
+{
+    [ValueConversion(typeof(bool[]), typeof(bool))]
+    public class NorBooleanConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            foreach (object value in values)
+                if (value == null || (bool)value)
+                    return false;
+
+            return true;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
